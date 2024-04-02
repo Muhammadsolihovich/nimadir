d9e0387bf7706aea732aef1674a970cee2ace2d6
Muhammadsolihovich <nurullohhakimjonov6161@gmail.com>
2024-04-02 19:33:19 +0500

    dd

---
+++ index.js
@@ -0,0 +1,123 @@
+// 1
+
+// let a=prompt("yoshingizni kiriting: ")
+// if(a>20){
+//     console.log("Xush kelibsiz");
+// }
+// else{
+//     console.log("Yoshingiz yetarli emas!");
+// }
+
+// 2
+
+// let a=prompt(" uchburchak tominini kiriting_1:")
+// let b=prompt(" uchburchak tominini kiriting_2:")
+// let c=prompt(" uchburchak tominini kiriting_3:")
+// if(a + b > c || b + c > a ||a + c > b){
+//     console.log("Bunday uchburchak mavjud");
+// }
+// else{
+//    console.log("Bunday uchburchak mavjud emas!"); 
+// }
+
+// 3
+
+// let a=prompt("Son kiriting: ")
+// let b=prompt("Son kiriting: ")
+// let c=prompt("Ishirani kiriting")
+// if(c = "plus"){
+//     console.log(a + b);
+// }
+// else if(c = "kop"){
+//     console.log(a * b);
+// }
+
+// 4
+
+// let a=prompt("Sonni kiriting: ")
+// if(a>0){
+//     console.log(Math.pow(a,1/2));
+// }
+// else if(a<0){
+//     console.log("Son manfiy");
+// }
+// else{
+//     console.log("sON 0 ga teng");
+// }
+
+// 5
+
+// let a=prompt("Sonni kiriting: ")
+// if(a>0 && Math.pow(a,1/2)==true){
+//     console.log(Math.pow(a,1/2));
+// }
+// else{
+//     console.log("son ildizdan butun chiqmaydi (son manfiy yoki 0)");
+// }
+
+// 6
+
+// let a=prompt("Yiol kiriting: ")
+// if(a%4==0){
+//     console.log("Yil kabisa yili");
+// }
+// else{
+// console.log("Yil kabisa emas");
+// }
+
+// 7
+
+
+// let a=prompt("Son kiriting: ")
+// if(a%15==0){
+//     console.log("FizzBuzz");
+// }
+// else if(a%5==0){
+//     console.log("Buzz");
+// }
+// else if(a%3==0){
+//     console.log("Fizz");
+// }
+// else{
+//     console.log("Adashdingiz");
+// }
+
+// 8
+
+// let a=prompt("Son kiriting_1: ")
+// let b=prompt("Son kiriting_2: ")
+// if(a%b==0 || b%a==0){
+//     console.log("SOnlar bir-biriga qoldiqsiz bolinadi");
+// }
+// else{
+//     console.log("Sonalr qoldiqsiz bolimaydi");
+// }
+
+// 9
+
+// let a=Number(prompt("Sonni kirit-1: "))
+// let b=Number(prompt("Sonni kirit-2: "))
+// let c=Number(prompt("Sonni kirit-3: "))
+// let A=(a+b+c)
+// if(A%3==0){
+//     console.log(A-0);
+// }
+// else{
+//     console.log("Son xato");
+// }
+
+// 10
+
+// let a=prompt("Son kiriting: ")
+// let A=Math.floor(a/10)
+// let B=Math.floor(a/100)
+// if(a%2==0 && A%2==0 && B%2==0 ){
+//     console.log("Sonning hamma raqamlar juft");
+// }
+// else{
+//     console.log("Son toq");
+// }
+
+
+
+
